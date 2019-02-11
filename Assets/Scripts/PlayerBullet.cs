66e5e910b311e857e4fec37cb50978ff1c9c6e53
Ace <ace.lowder@gmail.com>
2019-02-11 13:27:06 -0800

    Changed Template and Updated Player Bullet

--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -1,27 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerBullet : MonoBehaviour
 {
-    public Rigidbody2D rb2d;
     public float speed = 10f;
-    
-    // Start is called before the first frame update
+
+    protected Rigidbody2D m_Rigidbody2D;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        rb2d.velocity = new Vector2(0, 1) * speed;
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        m_Rigidbody2D.gravityScale = 0;
+        m_Rigidbody2D.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.gameObject.name.Contains("Enemy") && !col.gameObject.name.Contains("Bullet"))
+        if (col.gameObject.name.Contains("Enemy") && !col.gameObject.name.Contains("Bullet"))
         {
             float enemyPoints = 40;
 
